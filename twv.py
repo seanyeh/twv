62dfca3ebeb9fea48816188fdabccdcd0596be04
Sean Yeh <109418+seanyeh@users.noreply.github.com>
2019-08-04 09:57:19 -0500

    Use QtWebEngine instead of QtWebView

--- twv.py
+++ twv.py
@@ -3,12 +3,12 @@
 import PyQt5
 from PyQt5.QtCore import QUrl
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWebKitWidgets import QWebView
+from PyQt5.QtWebEngineWidgets import QWebEngineView
 
 import sys
 
-app = QApplication(sys.argv)
-view = QWebView()
+app = QApplication("twv")
+view = QWebEngineView()
 view.setUrl(QUrl(sys.argv[1]))
 view.showMaximized()
 app.exec_()
